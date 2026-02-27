9b31445880d465efbf3fac8d3b1cff09c9b27e71
AriPurwoAji <ajiaripurwo@gmail.com>
2026-02-27 15:55:47 +0700

    Update text counter

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Consumer<CounterModel>(
-      builder: (context, model, child) {
+      builder: (context, value, child) {
         return Scaffold(
           appBar: AppBar(
             // TRY THIS: Try changing the color here to a specific color (to
@@ -65,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 const Text('You have pushed the button this many times:'),
                 Text(
-                  '${_model.angka}',
+                  value.angka.toString(),
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
